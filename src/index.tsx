d31920081250aab47ddb726483c14ebc9d330aff
Alejo Boga <alejoboga20@gmail.com>
2022-01-21 11:19:35 -0300

    Creating customhook for useTranslation and creating intl provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import locales from 'locales/en.json';
+import flatten from 'flat';
+import { IntlProvider } from 'react-intl';
 import { App } from './App';
 import './styles/styles.scss';
 
+const DEFAULT_LANGUAGE: string = 'en';
+const messages = locales['en'];
+
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<IntlProvider messages={flatten(messages)} locale={'en'} defaultLocale={DEFAULT_LANGUAGE}>
+			<App />
+		</IntlProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
