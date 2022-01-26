26467474b85d76ea53cdf34d6f13fef2ba5f1ce2
Alejo Boga <alejoboga20@gmail.com>
2022-01-26 15:26:34 -0300

    Adapting input to receive error and be controlled by hook form

--- src/components/InputText/InputText.tsx
+++ src/components/InputText/InputText.tsx
@@ -1,16 +1,35 @@
+import { UseFormRegister } from 'react-hook-form';
 import styles from './InputText.module.scss';
 
-export const InputText = ({ label = '', placeholder = '' }: InputTextProps) => {
+export const InputText = ({
+	error = '',
+	label = '',
+	name,
+	placeholder = '',
+	register,
+	type = 'text',
+}: InputTextProps) => {
 	return (
 		<div className={styles.inputText}>
 			{label && <span className={styles.inputText__label}>{label}</span>}
 
-			<input className={styles.inputText__input} placeholder={placeholder} />
+			<input
+				className={`${styles.inputText__input} ${error && styles.inputText__inputError}`}
+				placeholder={placeholder}
+				type={type}
+				{...register(name)}
+			/>
+
+			{error && <small className={styles.inputText__error}>{error}</small>}
 		</div>
 	);
 };
 
 interface InputTextProps {
+	error?: string;
 	label?: string;
+	name: string;
 	placeholder?: string;
+	register: UseFormRegister<any>;
+	type?: 'text' | 'password' | 'number';
 }
