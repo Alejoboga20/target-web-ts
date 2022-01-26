8b82bc543e91ff1c045f8b0e599e1f599b227b0e
Alejo Boga <alejoboga20@gmail.com>
2022-01-26 15:26:04 -0300

    Adding prop and style to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,14 @@
 import styles from './Button.module.scss';
 
-export const Button = ({ label }: ButtonProps) => {
-	return <button className={styles.button}>{label}</button>;
+export const Button = ({ label, type = 'button' }: ButtonProps) => {
+	return (
+		<button className={styles.button} type={type}>
+			{label}
+		</button>
+	);
 };
 
 interface ButtonProps {
 	label: string;
+	type?: 'button' | 'reset' | 'submit';
 }
