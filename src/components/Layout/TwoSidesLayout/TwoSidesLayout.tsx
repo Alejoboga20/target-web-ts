b12298fa662b5e503b9eff6bc928114047332fa7
Alejo Boga <alejoboga20@gmail.com>
2022-06-13 11:45:09 -0300

    Create custom hook to handle outside clicks

--- src/components/Layout/TwoSidesLayout/TwoSidesLayout.tsx
+++ src/components/Layout/TwoSidesLayout/TwoSidesLayout.tsx
@@ -1,9 +1,10 @@
-import { DefaultRightElement } from 'components';
+import { BurgerMenu, DefaultRightElement } from 'components';
 import styles from './TwoSidesLayout.module.scss';
 
 export const TwoSidesLayout = ({ rightElement, leftElement }: TwoSidesLayoutProps) => {
 	return (
 		<div className={styles.layout}>
+			<BurgerMenu />
 			<div className={styles.layout__left}>{leftElement}</div>
 			<div className={styles.layout__right}>
 				{rightElement ? rightElement : <DefaultRightElement />}
