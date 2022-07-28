92fd3d8a3965b728541510d3f8cf8885f710af83
Alejo Boga <alejoboga20@gmail.com>
2022-07-28 10:21:53 -0300

    Create about page

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,9 +1,27 @@
-import { TwoSidesLayout } from 'components';
+import { Link } from 'react-router-dom';
+
+import { useTranslation } from 'hooks';
+import { Button, TwoSidesLayout } from 'components';
+import { routePaths } from 'router';
+import { ReactComponent as Smilies } from 'assets/smilies.svg';
+import styles from './AboutPage.module.scss';
 
 export const AboutPage = () => {
+	const t = useTranslation();
+
 	return (
 		<TwoSidesLayout>
-			<h1>About Page</h1>
+			<div className={styles.about}>
+				<Smilies />
+
+				<h3 className={styles.about__heading}>{t('about.title')}</h3>
+
+				<p className={styles.about__description}>{t('about.description')}</p>
+
+				<Link to={routePaths.signup}>
+					<Button label={t('about.button')} />
+				</Link>
+			</div>
 		</TwoSidesLayout>
 	);
 };
