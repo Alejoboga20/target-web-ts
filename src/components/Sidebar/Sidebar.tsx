5c34ab141b208ae07621db7b2caea766be43651b
Alejo Boga <alejoboga20@gmail.com>
2022-09-06 16:52:48 -0400

    Implement component render based on state

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
+
 import { Welcome, Empty } from '../';
 
+type userState = 'welcome' | 'empty';
+
+const Component = {
+	welcome: <Welcome />,
+	empty: <Empty />,
+};
+
 export const Sidebar = () => {
-	return <Empty />;
+	const [state, setState] = useState<userState>('welcome');
+
+	return Component[state];
 };
